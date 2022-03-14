a91c288125d573e6d5a8e092446fc74860936bc2
Your <gorelikspb@ya.ru>
2022-03-14 12:41:31 +0300

    feat: new transaction

---
+++ blockchain.py
@@ -0,0 +1,38 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        
+    def new_block(self):
+        # Создает новый блок и вносит его в цепь
+        pass
+    
+  
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Направляет новую транзакцию в следующий блок
+ 
+        :param sender: <str> Адрес отправителя
+        :param recipient: <str> Адрес получателя
+        :param amount: <int> Сумма
+        :return: <int> Индекс блока, который будет хранить эту транзакцию
+        """
+ 
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+ 
+        return self.last_block['index'] + 1
+    
+    @staticmethod
+    def hash(block):
+        # Хеширует блок
+        pass
+ 
+    @property
+    def last_block(self):
+        # Возвращает последний блок в цепочке
+        pass
